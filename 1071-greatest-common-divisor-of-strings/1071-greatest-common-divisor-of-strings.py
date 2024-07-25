fde8d9ffc453aee73828f70d1c3296fc99e3d8de
Devikagouri <115675522+Devikagouri@users.noreply.github.com>
2024-07-25 16:59:48 +0530

    Time: 393 ms (5.14%), Space: 13.9 MB (7.59%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        l1 = len(str1)
+        l2 = len(str2)
+        if(l1>l2):
+            low = str2
+            high = str1
+            h = l1
+            l = l2
+        else:
+            low = str1
+            high = str2
+            h = l2
+            l = l1
+        
+        st = ''
+        for i in range(h):
+            j = 1
+            st = low[0:(l-i)]
+            s = ''
+            print(st)
+            print(i,j)
+            while(len(s)<=h and st!=''):                
+                s = st*j
+                print(st)
+                if(s==high):
+                    k = 1
+                    while(k<=l):
+                        if((st*k)==low):
+                            return st
+                        k+=1
+                print(i,j)
+                j+=1
+        return ''
